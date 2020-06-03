1f0e342d13fb0b5815e0284bc9b9e076a424e86f
Nicolas Schad <nico_schadoc@hotmail.fr>
2020-06-03 14:23:20 +0200

    Updates

---
+++ precache-manifest.4fba0e11dac10f13dcc8ed89e63385e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45d763ba425f60e15a11d002b291245",
+    "url": "/Geoquiz/index.html"
+  },
+  {
+    "revision": "0adc4cc5f15e7fd7b34b",
+    "url": "/Geoquiz/static/css/main.c57b4373.chunk.css"
+  },
+  {
+    "revision": "8b64074883d4d7bd2cc9",
+    "url": "/Geoquiz/static/js/2.9c469ee4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Geoquiz/static/js/2.9c469ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0adc4cc5f15e7fd7b34b",
+    "url": "/Geoquiz/static/js/main.d20ae63b.chunk.js"
+  },
+  {
+    "revision": "c15d25a07496a9450e9c",
+    "url": "/Geoquiz/static/js/runtime-main.17895595.js"
+  },
+  {
+    "revision": "bf0ad3a2e84c09472cc320b9ba96d071",
+    "url": "/Geoquiz/static/media/france.bf0ad3a2.svg"
+  }
+]);
